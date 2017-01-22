4ae37f8c628e697d6c88857eef5d80f2b731da17
Md. Nadimozzaman Pappo <mnpappo@gmail.com>
2017-01-22 23:10:57 +0600

    final template ready

--- jinja_test.py
+++ jinja_test.py
@@ -1,7 +1,30 @@
-#using jinja2 templating 
+#using jinja2 templating
 from docxtpl import DocxTemplate
+from faker import Faker
 
 doc = DocxTemplate("template/docTemplate-1.docx")
-context = { 'company_name' : "World company", }
-doc.render(context)
-doc.save("generated/generated_doc.docx")
+fake = Faker()
+
+def escaper(unicodeList):
+    newList = [ s.encode('utf8') for s in unicodeList ]
+    finalPara = ""
+    for s in newList:
+        finalPara = finalPara + s
+    return finalPara
+
+for inc in range(3):
+    context = {
+        'title_h1_bold' : fake.sentence(nb_words=6, variable_nb_words=True),
+        'title_h1_normal' : fake.sentence(nb_words=4, variable_nb_words=True),
+        'name' : fake.name(),
+        'email' : fake.email(),
+        'phone' : fake.phone_number(),
+        'date' : fake.date(pattern="%Y-%m-%d"),
+        'para_one' : escaper(fake.paragraphs(nb=4)),
+        'single_sentence_one' : fake.sentence(nb_words=7, variable_nb_words=True),
+        'single_sentence_two' : fake.sentence(nb_words=9, variable_nb_words=True),
+        'single_sentence_three' : fake.sentence(nb_words=5, variable_nb_words=True),
+        'para_two' : escaper(fake.paragraphs(nb=5)),
+        }
+    doc.render(context)
+    doc.save("generated/generated_doc_{id}.docx".format(id=inc))
